c204ef48ce0207c6be8fb4004c73bf9f270c265c
thebhaveshshinde <thebhaveshshinde@gmail.com>
2024-07-06 07:23:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+var tr = document.createElement('tr')
+
+var th = document.createElement('th')
+th.innerText="Name"
+tr.appendChild(th)
+
+var th = document.createElement('th')
+th.innerText="Age"
+tr.appendChild(th)
+
+var th = document.createElement('th')
+th.innerText="Salary"
+tr.appendChild(th)
+
+document.getElementById('TABLE').appendChild(tr)
+
+
+
+
+var tr = document.createElement('tr')
+
+var td = document.createElement('td')
+td.innerText="Bhavesh"
+tr.appendChild(td)
+
+var td = document.createElement('td')
+td.innerText="21"
+tr.appendChild(td)
+
+var td = document.createElement('td')
+td.innerText="20000"
+tr.appendChild(td)
+
+document.getElementById('TABLE').appendChild(tr)
+
+
+
+
+var tr = document.createElement('tr')
+
+var td = document.createElement('td')
+td.innerText="Arun"
+tr.appendChild(td)
+
+var td = document.createElement('td')
+td.innerText="41"
+tr.appendChild(td)
+
+var td = document.createElement('td')
+td.innerText="40000"
+tr.appendChild(td)
+
+document.getElementById('TABLE').appendChild(tr)
+
+
+
+
+var tr = document.createElement('tr')
+
+var td = document.createElement('td')
+td.innerText="Shinde"
+tr.appendChild(td)
+
+var td = document.createElement('td')
+td.innerText="61"
+tr.appendChild(td)
+
+var td = document.createElement('td')
+td.innerText="60000"
+tr.appendChild(td)
+
+document.getElementById('TABLE').appendChild(tr)
+
+
